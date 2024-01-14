121d4fea667abad9b43c79612a74ee6d2930ccf8
joseescobara <105508880+joseescobara@users.noreply.github.com>
2024-01-14 02:13:34 -0500

    Delete 5. Simulación Nu's del modelo directory

--- 5. Simulación Nu's del modelo/13.Simulacion_nu_posterior_slash_model.R
+++
@@ -1,8 +0,0 @@
-#simulacion del nu de su distribucion posterior
-nu_modelo_slash <- function(a = 6, b = 2, pesos){
-  n <- length(pesos) #numero de individuos
-  ln_pesos <- log(pesos) #logaritmo de los pesos
-  muestra <- rgamma(1, shape = a + n, rate = b - sum(ln_pesos))
-  return(muestra)
-}
-
